6b5f661cc7f1bae08783d4b97061bba9055f708e
ike005 <154865516+ike005@users.noreply.github.com>
2024-03-22 10:36:42 -0400

    Add files via upload

---
+++ basic animation blackbelt.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 16:36:21 2024
+
+@author: chibuike
+"""
+import pygame
+import random
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+class Charlie(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        
+        # set up the image
+        self.image = pygame.image.load("elephant.png")
+        self.image.convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+
+        #create the corresponding rect
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 320
+        self.rect.centery = 240
+        
+        #create the ability to move
+        self.dx = 5
+        self.dy = 3
+        
+    def update(self):
+        self.rect.right += self.dx
+        if self.rect.centerx >= screen.get_width():
+            self.rect.centerx = screen.get_width()
+            self.dx = -self.dx + random.randint(-10,10)
+            if self.dx > 25:
+                self.dx = 5
+            elif self.dx < -25:
+                self.dx = -5
+            self.changeimage()
+        elif self.rect.centerx <= 0:
+            self.rect.centerx = 0
+            self.dx = -self.dx + random.randint(-10,10)
+            if self.dx > 25:
+                self.dx = 5
+            elif self.dx < -25:
+                self.dx = -5
+            self.changeimage()
+        self.rect.top += self.dy
+        if self.rect.centery >= screen.get_height():
+            self.rect.centery = screen.get_height()
+            self.dy = -self.dy + random.randint(-10,10)
+            if self.dy > 25:
+                self.dy = 5
+            elif self.dy < -25:
+                self.dy = -5
+            self.changeimage()
+        elif self.rect.centery <= 0:
+            self.rect.centery = 0
+            self.dy = -self.dy + random.randint(-10,10)
+            if self.dy > 25:
+                self.dy = 5
+            elif self.dy < -25:
+                 self.dy = -5
+            self.changeimage()
+        # check bounds
+        
+    def changeimage(self):
+        fileName = ("giraffe.png", "monkey.png", "hippo.png", "panda.png", "parrot.png", "penguin.png", "pig.png", "rabbit.png", "snake.png")
+        randNum = random.randrange(len(fileName))
+        
+        self.image = pygame.image.load(fileName[randNum])
+        self.image.convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        
+    
+        
+            
+def main():
+    
+    #set up screen
+    pygame.display.set_caption("Basic sprite demo")
+    background = pygame.image.load("sample.png")
+    background.convert_alpha()
+    background = pygame.transform.scale(background,screen.get_size())
+    screen.blit(background, (0, 0))
+    
+    # instantiate charlie
+    charlie = Charlie()
+    allSprites = pygame.sprite.Group(charlie)
+    
+    # set up timing
+    clock = pygame.time.Clock()
+    keepGoing = True
+    while(keepGoing):
+        clock.tick(30)
+        
+        # check events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+                
+        # clear and redraw sprites
+        allSprites.clear(screen, background)
+        allSprites.update()
+        allSprites.draw(screen)
+        
+        pygame.display.flip()
+        
+        
+if __name__ == "__main__":
+    main()
+    pygame.quit()   
